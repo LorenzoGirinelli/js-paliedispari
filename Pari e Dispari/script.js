7f3961e905f69b281ddd691979ce2eecdfbae44f
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2021-11-22 17:16:59 +0100

    somma

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -3,20 +3,20 @@ const userEvenorOdd = prompt(`Scegli tra pari o dispari`);
 console.log(userEvenorOdd);
 
 // SCEGLIERE UN NUMERO
-const userNumber = prompt(`Scegli un numero`);
+const userNumber = prompt(`Scegli un numero da 1 a 5`);
 console.log(userNumber);
 
 // NUMERO GENERATO DAL COMPUTER
-const computerNumber = Math.floor(Math.random() * 100) + 1;
+const computerNumber = Math.floor(Math.random() * 5) + 1;
 console.log(computerNumber.toFixed());
 
 // SOMMA TRA I DUE NUMERI
-let sum = sommaNumeri(userNumber + computerNumber);
+let sum = sommaNumeri(userNumber, computerNumber);
 console.log('Risultato funzione', sum);
 
-// function sommaNumeri(userNumber, computerNumber) {
-//     const somma = userNumber + computerNumber;
-//     // console.log(sommaNumeri);
-//     return somma;
-// }
+function sommaNumeri(userNumber, computerNumber) {
+    const somma = userNumber + computerNumber;
+    // console.log(somma);
+    return somma;
+}
 // VEDERE SE E' PARI O DISPARI
